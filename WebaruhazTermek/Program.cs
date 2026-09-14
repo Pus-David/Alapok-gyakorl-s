08ba98e62f16dd0fc3a11ae5df14bc7a5316ed47
Pus-David <puskas.david.2022d@telekimezotur.hu>
2026-09-14 11:52:42 +0200

    yesnt

--- WebaruhazTermek/Program.cs
+++ WebaruhazTermek/Program.cs
@@ -1,10 +1,31 @@
 ﻿using WebaruhazTermek;
 
-Termek termek = new Termek();
-termek.Nev = "Alma";
-termek.Egysegar = 100;
-termek.Keszlet = 50;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Termek termek1 = new Termek();
 
-termek.Nev = "Körte";
-termek.Egysegar = -50; 
-termek.Keszlet = -10; 
+        termek1.Nev = "Laptop";
+        termek1.Egysegar = 250000;
+        termek1.Keszlet = 10;
+
+        Termek termek2 = new Termek();
+
+        termek2.Nev = "Egér";
+        termek2.Egysegar = -500;
+        termek2.Keszlet = -3;
+
+        Console.WriteLine("1. termék:");
+        Console.WriteLine("Név: " + termek1.Nev);
+        Console.WriteLine("Egységár: " + termek1.Egysegar + " Ft");
+        Console.WriteLine("Készlet: " + termek1.Keszlet + " db");
+
+        Console.WriteLine();
+
+        Console.WriteLine("2. termék:");
+        Console.WriteLine("Név: " + termek2.Nev);
+        Console.WriteLine("Egységár: " + termek2.Egysegar + " Ft");
+        Console.WriteLine("Készlet: " + termek2.Keszlet + " db");
+    }
+}
--- WebaruhazTermek/Termek.cs
+++ WebaruhazTermek/Termek.cs
@@ -4,28 +4,34 @@ using System.Text;
 
 namespace WebaruhazTermek
 {
-    internal class Termek
+    public class Termek
     {
         public string Nev { get; set; }
-        public int Egysegar 
+
+        private int egysegar;
+        public int Egysegar
         {
-            get { return Egysegar; }
+            get { return egysegar; }
             set
             {
-                if (value < 0) Egysegar = 1;
+                if (value < 1)
+                    egysegar = 1;
+                else
+                    egysegar = value;
             }
         }
 
+        private int keszlet;
         public int Keszlet
         {
-            get { return Keszlet; }
+            get { return keszlet; }
             set
             {
-                if (value < 0) Keszlet = 0;
+                if (value < 0)
+                    keszlet = 0;
+                else
+                    keszlet = value;
             }
         }
-
-
-
     }
 }
