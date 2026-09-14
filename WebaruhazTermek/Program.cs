3cc7454bd1606b1f8a479627628aef0c8263a094
Pus-David <puskas.david.2022d@telekimezotur.hu>
2026-09-14 10:35:50 +0200

    1. feladat kiserlet

---
+++ WebaruhazTermek/Program.cs
@@ -0,0 +1,10 @@
+﻿using WebaruhazTermek;
+
+Termek termek = new Termek();
+termek.Nev = "Alma";
+termek.Egysegar = 100;
+termek.Keszlet = 50;
+
+termek.Nev = "Körte";
+termek.Egysegar = -50; 
+termek.Keszlet = -10; 
---
+++ WebaruhazTermek/Termek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebaruhazTermek
+{
+    internal class Termek
+    {
+        public string Nev { get; set; }
+        public int Egysegar 
+        {
+            get { return Egysegar; }
+            set
+            {
+                if (value < 0) Egysegar = 1;
+            }
+        }
+
+        public int Keszlet
+        {
+            get { return Keszlet; }
+            set
+            {
+                if (value < 0) Keszlet = 0;
+            }
+        }
+
+
+
+    }
+}
