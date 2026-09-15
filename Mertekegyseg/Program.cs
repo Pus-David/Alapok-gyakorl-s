a26a996ef3567f5316ef9890ac231dfd3e4baf2b
Pus-David <puskas.david.2022d@telekimezotur.hu>
2026-09-15 09:07:13 +0200

    multoravege

---
+++ Mertekegyseg/Program.cs
@@ -0,0 +1,19 @@
+﻿using Mertekegyseg;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        double meter = Atvalto.KmBolMeter(5);
+
+        Console.WriteLine("5 km = " + meter + " méter");
+
+        Atvalto atvalto = new Atvalto();
+
+        atvalto.Szorzo = 100;
+
+        double eredmeny = atvalto.EgyediAtvaltas(3);
+
+        Console.WriteLine("3 * 100 = " + eredmeny);
+    }
+}
