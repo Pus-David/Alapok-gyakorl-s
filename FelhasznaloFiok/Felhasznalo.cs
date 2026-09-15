52b4a8e1d5d7256321539198159fc232dbcd5497
Pus-David <puskas.david.2022d@telekimezotur.hu>
2026-09-15 10:36:31 +0200

    felhasznalo vege

--- FelhasznaloFiok/Felhasznalo.cs
+++ FelhasznaloFiok/Felhasznalo.cs
@@ -6,5 +6,21 @@ namespace FelhasznaloFiok
 {
     internal class Felhasznalo
     {
+        private string jelszo = "123456";
+
+        public string Felhasznalonev { get; set; }
+
+        public void JelszoModositas(string regiJelszo, string ujJelszo)
+        {
+            if (regiJelszo == jelszo && ujJelszo.Length >= 6)
+            {
+                jelszo = ujJelszo;
+                Console.WriteLine("A jelszó sikeresen módosítva!");
+            }
+            else
+            {
+                Console.WriteLine("A jelszó módosítása sikertelen!");
+            }
+        }
     }
 }
--- FelhasznaloFiok/Program.cs
+++ FelhasznaloFiok/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿usning felhasznaloFiok;
+
+static void Main(string[] args)
+{
+    Felhasznalo felhasznalo = new Felhasznalo();
+
+    felhasznalo.Felhasznalonev = "TesztElek";
+
+    Console.WriteLine($"Felhasználónév: {felhasznalo.Felhasznalonev}");
+
+    felhasznalo.JelszoModositas("123456", "abcdef");
+
+    felhasznalo.JelszoModositas("hibas", "qwerty");
+
+    felhasznalo.JelszoModositas("abcdef", "123");
+
+    felhasznalo.JelszoModositas("abcdef", "ujjelszo123");
+}
